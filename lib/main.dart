ee60ba42f8fa284201cb61ddf0d35acb122e77f1
DairikuYoko <yokoriku@gmail.com>
2025-11-28 00:43:23 +0900

    setStateの確認(スライダーで状態の変更を確認)

--- lib/main.dart
+++ lib/main.dart
@@ -16,52 +16,45 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyCheckBox(),
+      home: MySlider(),
     );
   }
 }
 
-class MyCheckBox extends StatefulWidget {
-  const MyCheckBox({super.key});
+class MySlider extends StatefulWidget {
+  const MySlider({super.key});
 
   @override
-  State<MyCheckBox> createState() => _MyCheckBoxState();
+  State<MySlider> createState() => _MySliderState();
 }
 
-class _MyCheckBoxState extends State<MyCheckBox> {
-  bool isChecked = false;
-
-  /// isCheckedの値を反転するメソッド
-  /// trueなら、falseに
-  /// falseなら、trueに
-  void _toggleCheckBox() {
-    setState(() {
-      isChecked = !isChecked;
-    });
-  }
+class _MySliderState extends State<MySlider> {
+  double _currentSliderValue = 20;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('MyCheckBox Demo'),
+        title: Text('Flutter Slider Demo'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 70),
-              child: CheckboxListTile(
-                  controlAffinity: ListTileControlAffinity.leading,
-                  title: const Text('利用規約に同意する。'),
-                  value: isChecked,
-                  onChanged: (value) {
-                    _toggleCheckBox();
-                  }),
-            ),
-            Text('isCheckedの状態 = [$isChecked]')
+            Slider(
+                value: _currentSliderValue,
+                min: 0,
+                max: 100,
+                divisions: 100,
+                label: _currentSliderValue.round().toString(),
+                onChanged: (double value) {
+                  setState(() {
+                    _currentSliderValue = value;
+                  });
+                  print('value Selected ${_currentSliderValue.round()}');
+                }),
+            Text('Value Selectted ${_currentSliderValue.round()}')
           ],
         ),
       ),
